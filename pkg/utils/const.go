1fc6b34823181138c27fc59c838fec70883ce851
Sudharma Puranik <sudharma.puranik@leanix.net>
2022-05-18 10:51:28 +0200

    [HEL-1859]: add constants to separate package

---
+++ pkg/utils/const.go
@@ -0,0 +1,19 @@
+package utils
+
+const (
+	EnableCustomStorageFlag          string = "enable-custom-storage"
+	StorageBackendFlag               string = "storage-backend"
+	AzureAccountNameFlag             string = "azure-account-name"
+	AzureAccountKeyFlag              string = "azure-account-key"
+	AzureContainerFlag               string = "azure-container"
+	LocalFilePathFlag                string = "local-file-path"
+	VerboseFlag                      string = "verbose"
+	IntegrationAPIDatasourceNameFlag string = "integration-api-datasourcename"
+	IntegrationAPIFqdnFlag           string = "integration-api-fqdn"
+	IntegrationAPITokenFlag          string = "integration-api-token"
+	BlacklistNamespacesFlag          string = "blacklist-namespaces"
+	LxWorkspaceFlag                  string = "lx-workspace"
+	LocalFlag                        string = "local"
+	IrisFlag                         string = "enable-v2-integration-api"
+	ConfigurationNameFlag            string = "configuration-name"
+)
