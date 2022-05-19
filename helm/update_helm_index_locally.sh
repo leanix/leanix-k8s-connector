482a1c9ddd30d44d84244a1335afbde4acd0f1ee
Dennis Berthold <dennis.berthold@leanix.net>
2022-05-19 12:18:04 +0200

    HEL-1891 Release action creates pull request for updated helm chart

--- helm/update_helm_index_locally.sh
+++ helm/update_helm_index_locally.sh
@@ -6,14 +6,14 @@ set -e
 
 # Creates or updates a new Helm chart package
 # Specific semantic version
-sed -i '' -E 's/(tag:).*/tag: '"$SPECIFIC_VERSION"'/g' ./leanix-k8s-connector/values.yaml
-sed -i '' -E 's/(version:).*/version: '"$SPECIFIC_VERSION"'/g' ./leanix-k8s-connector/Chart.yaml
-sed -i '' -E 's/(appVersion:).*/appVersion: '"\"$SPECIFIC_VERSION\""'/g' ./leanix-k8s-connector/Chart.yaml
+sed -i '' -E 's/(tag:).*/tag: '"$SPECIFIC_VERSION"'/g' leanix-k8s-connector/values.yaml
+sed -i '' -E 's/(version:).*/version: '"$SPECIFIC_VERSION"'/g' leanix-k8s-connector/Chart.yaml
+sed -i '' -E 's/(appVersion:).*/appVersion: '"\"$SPECIFIC_VERSION\""'/g' leanix-k8s-connector/Chart.yaml
 helm package ./leanix-k8s-connector
 
 # Latest version/chart
-sed -i '.bak' -E 's/(tag:).*/tag: '"$LATEST_BASE_VERSION"'.latest/g' ./leanix-k8s-connector/values.yaml
-helm package --version="$LATEST_BASE_VERSION".0.0-latest --app-version="$LATEST_BASE_VERSION".0.0-latest ./leanix-k8s-connector
+sed -i '.bak' -E 's/(tag:).*/tag: '"$LATEST_BASE_VERSION"'.latest/g' leanix-k8s-connector/values.yaml
+helm package --version="$LATEST_BASE_VERSION".0.0-latest --app-version="$LATEST_BASE_VERSION".0.0-latest leanix-k8s-connector
 rm leanix-k8s-connector/values.yaml
 mv leanix-k8s-connector/values.yaml.bak leanix-k8s-connector/values.yaml
 
