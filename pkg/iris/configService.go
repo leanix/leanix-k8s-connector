4e2bc2b9fb86482856499a021a6b5db366fa56bf
Dennis Berthold <dennis.berthold@leanix.net>
2023-02-17 15:37:32 +0100

    HEL-2444 Introducing a configService to interact with iris configurations

---
+++ pkg/iris/configService.go
@@ -0,0 +1,28 @@
+package iris
+
+import (
+	"github.com/leanix/leanix-k8s-connector/pkg/iris/models"
+)
+
+type ConfigService interface {
+	GetConfiguration(configurationName string) ([]byte, error)
+	GetScanResults(configurationId string) ([]models.IrisResultItem, error)
+}
+
+type configService struct {
+	irisApi API
+}
+
+func NewConfigService(irisApi API) ConfigService {
+	return &configService{
+		irisApi: irisApi,
+	}
+}
+
+func (a *configService) GetConfiguration(configurationName string) ([]byte, error) {
+	return a.irisApi.GetConfiguration(configurationName)
+}
+
+func (a *configService) GetScanResults(configurationId string) ([]models.IrisResultItem, error) {
+	return a.irisApi.GetScanResults(configurationId)
+}
