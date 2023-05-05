d64917b963d2d4d0bdde5dc0d7cd75d3f7e05e36
rajeshmohanty-leanix <rajesh.mohanty@leanix.net>
2023-05-05 13:38:53 +0200

    HEL-2571 Updated mapping_test.go

--- pkg/iris/workloads/services/mapper/mapping_test.go
+++ pkg/iris/workloads/services/mapper/mapping_test.go
@@ -4,11 +4,13 @@ import (
 	"github.com/leanix/leanix-k8s-connector/pkg/kubernetes"
 	"github.com/stretchr/testify/assert"
 	appsv1 "k8s.io/api/apps/v1"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/utils/pointer"
 	"testing"
 	"time"
 )
@@ -130,6 +132,134 @@ func Test_MapWorkloads_success(t *testing.T) {
 			Status: appsv1.DeploymentStatus{
 				Replicas:      1,
 				ReadyReplicas: 1,
+			},
+		},
+		&batchv1.CronJob{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:              "test-cronjob-1",
+				Namespace:         "cronjob-1-namespace",
+				CreationTimestamp: metav1.Date(2019, 01, 12, 8, 55, 20, 0, time.UTC),
+				Labels: map[string]string{
+					"name": "service-1",
+					"failure-domain.beta.kubernetes.io/region": "westeurope",
+					"failure-domain.beta.kubernetes.io/zone":   "2",
+					"beta.kubernetes.io/instance-type":         "Standard_D8s_v3",
+				},
+				Annotations: map[string]string{
+					"deployment.kubernetes.io/revision": "1",
+				},
+			},
+			Spec: batchv1.CronJobSpec{
+				Schedule: "0 0 * * *",
+				JobTemplate: batchv1.JobTemplateSpec{
+					Spec: batchv1.JobSpec{
+						Selector: &metav1.LabelSelector{
+							MatchLabels: map[string]string{"app": "service-2"},
+						},
+						Template: corev1.PodTemplateSpec{
+							Spec: corev1.PodSpec{
+								Containers: []corev1.Container{
+									{
+										Resources: corev1.ResourceRequirements{
+											Limits: corev1.ResourceList{
+												corev1.ResourceCPU:    *resource.NewQuantity(int64(100), "Mi"),
+												corev1.ResourceMemory: *resource.NewQuantity(int64(50), "m"),
+											},
+											Requests: corev1.ResourceList{
+												corev1.ResourceCPU:    *resource.NewQuantity(int64(100), "Mi"),
+												corev1.ResourceMemory: *resource.NewQuantity(int64(50), "m"),
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		&appsv1.StatefulSet{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:              "test-statefulset-1",
+				Namespace:         "statefulset-1-namespace",
+				CreationTimestamp: metav1.Date(2019, 01, 12, 8, 55, 20, 0, time.UTC),
+				Labels: map[string]string{
+					"name": "service-1",
+					"failure-domain.beta.kubernetes.io/region": "westeurope",
+					"failure-domain.beta.kubernetes.io/zone":   "2",
+					"beta.kubernetes.io/instance-type":         "Standard_D8s_v3",
+				},
+				Annotations: map[string]string{
+					"deployment.kubernetes.io/revision": "1",
+				},
+			},
+			Spec: appsv1.StatefulSetSpec{
+				Replicas:    pointer.Int32Ptr(1),
+				ServiceName: "test-service-1",
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{"app": "service-1"},
+				},
+				Template: corev1.PodTemplateSpec{
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Resources: corev1.ResourceRequirements{
+									Limits: corev1.ResourceList{
+										corev1.ResourceCPU:    *resource.NewQuantity(int64(100), "Mi"),
+										corev1.ResourceMemory: *resource.NewQuantity(int64(50), "m"),
+									},
+									Requests: corev1.ResourceList{
+										corev1.ResourceCPU:    *resource.NewQuantity(int64(100), "Mi"),
+										corev1.ResourceMemory: *resource.NewQuantity(int64(50), "m"),
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			Status: appsv1.StatefulSetStatus{
+				Replicas:      1,
+				ReadyReplicas: 1,
+			},
+		},
+		&appsv1.DaemonSet{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:              "test-daemonset-1",
+				Namespace:         "daemonset-1-namespace",
+				CreationTimestamp: metav1.Date(2019, 01, 12, 8, 55, 20, 0, time.UTC),
+				Labels: map[string]string{
+					"name": "service-1",
+					"failure-domain.beta.kubernetes.io/region": "westeurope",
+					"failure-domain.beta.kubernetes.io/zone":   "2",
+					"beta.kubernetes.io/instance-type":         "Standard_D8s_v3",
+				},
+				Annotations: map[string]string{
+					"deployment.kubernetes.io/revision": "1",
+				},
+			},
+			Spec: appsv1.DaemonSetSpec{
+				Selector: &metav1.LabelSelector{
+					MatchLabels: map[string]string{"app": "service-1"},
+				},
+				Template: corev1.PodTemplateSpec{
+					Spec: corev1.PodSpec{
+						Containers: []corev1.Container{
+							{
+								Resources: corev1.ResourceRequirements{
+									Limits: corev1.ResourceList{
+										corev1.ResourceCPU:    *resource.NewQuantity(int64(100), "Mi"),
+										corev1.ResourceMemory: *resource.NewQuantity(int64(50), "m"),
+									},
+									Requests: corev1.ResourceList{
+										corev1.ResourceCPU:    *resource.NewQuantity(int64(100), "Mi"),
+										corev1.ResourceMemory: *resource.NewQuantity(int64(50), "m"),
+									},
+								},
+							},
+						},
+					},
+				},
 			},
 		},
 	}
