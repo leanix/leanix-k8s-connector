689ee1160e21efd6a43805f6b32b3f7415cb0cad
timmeyerLeanIX <tim.meyer@leanix.net>
2023-06-09 15:14:07 +0200

    add logging for deployments

--- pkg/kubernetes/deployments.go
+++ pkg/kubernetes/deployments.go
@@ -3,6 +3,7 @@ package kubernetes
 import (
 	"context"
 
+	"github.com/leanix/leanix-k8s-connector/pkg/logger"
 	v1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -10,6 +11,7 @@ import (
 // Deployments gets the list of deployments in a namespace
 func (k *API) Deployments(namespace string) (*v1.DeploymentList, error) {
 	deployments, err := k.Client.AppsV1().Deployments(namespace).List(context.Background(), metav1.ListOptions{})
+	logger.Infof("list of workload deployments: %v", deployments)
 	if err != nil {
 		return nil, err
 	}
