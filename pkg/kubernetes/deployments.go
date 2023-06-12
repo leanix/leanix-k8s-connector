2cf845ac0ea639aed0457cd828fee721e3e0fe5e
timmeyerLeanIX <tim.meyer@leanix.net>
2023-06-12 10:36:28 +0200

    Update logging to exclude secrets

--- pkg/kubernetes/deployments.go
+++ pkg/kubernetes/deployments.go
@@ -11,9 +11,11 @@ import (
 // Deployments gets the list of deployments in a namespace
 func (k *API) Deployments(namespace string) (*v1.DeploymentList, error) {
 	deployments, err := k.Client.AppsV1().Deployments(namespace).List(context.Background(), metav1.ListOptions{})
-	logger.Infof("list of workload deployments: %v", deployments)
 	if err != nil {
 		return nil, err
+	}
+	for _, deployment := range deployments.Items {
+		logger.Infof("Deployment: name=%s, namespace=%s, labels=%v", deployment.Name, deployment.Namespace, deployment.Labels)
 	}
 	return deployments, nil
 }
